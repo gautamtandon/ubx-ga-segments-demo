098559a7fb6676621919e14d79516e87eccdddd7
Gautam Tandon <gautam.tandon@ellipsissolutions.com>
2016-03-24 18:10:16 -0700

    updated README and cleaned up code

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-tbd
+## IBM UBX Google Analytics Segments Connector
+This connectors picks up custom segments created in Google Analytics and pushes them over to UBX. The segments can then be mapped to the already existing segments in IBM Engage to share segments data between Google Analytics and IBM Engage. A typical use case would be to do targeted email marketing based on website activity of logged in (or known) users.
+
+### How it works
+In "IBM language", this is a "push type segment" connector. Which means, it pushes data to UBX instead of waiting for UBX to make a call. Because of that, you can simply run this connector via command line instead of having to setup a HTTP endpoint where IBM UBX could call. Having said that, based on your particular situation, you might need a "pull type" connector, which is going to be pretty similar except for the fact that instead of you running the connector every so often, you'd wait UBX to make a call and do the same stuff.
+
+This is how this connector works at a high level:
+=================================================
+1. Pulls custom segments from Google Analytics.
+2. Figures out which segments need to be pushed to UBX (i.e. ignore the ones that have already been pushed).
+3. Pushes the new segments to IBM UBX.
+4. Deletes the segments from IBM UBX that don't exist in Google Analytics anymore.
+
+### Setup
+
+Pre-requisites
+==============
+1. Clear understanding of IBM UBX
+2. Clear understanding of Google Analytics APIs
+
+Steps
+=====
+1. Create a Google Analytics App on Google Developer Console.
+2. Create a 'Segments Push Type' Custom Endpoint in UBX. Keep in mind if you are creating a custom endpoint for the first time, you might need to get your company provisioned into the IBM UBX system.
+3. Edit the "ellipsis-google.php" file and set the following global variables with the right values.
+
+	$ga_appname = "";
+	$ga_p12_filepath = "";
+	$ga_devacct = "";
+
+	$ubx_es_endpoint_id = 0;
+	$ubx_es_endpoint_key = "";
+	$ubx_shiro_key = ""
+
+### Usage
+Simply configure the script 'ellipsis-google.php' to be called every so often - perhaps as a cron job, and you are all set! Now create a segment in GA and go to your UBX console and refresh the segments, and you'll see it there!
--- ellipsis-google.php
+++ ellipsis-google.php
@@ -1,5 +1,13 @@
 <?php
 
+$ga_appname = "";
+$ga_p12_filepath = "";
+$ga_devacct = "";
+
+$ubx_es_endpoint_id = 0;
+$ubx_es_endpoint_key = "";
+$ubx_shiro_key = ""
+
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/google-api-php-client/src/');
 include_once "Google/Client.php";
 
@@ -7,11 +15,13 @@ $db = new SQLite3('es_ga_sqlitedb.db');
 $db->query('create table if not exists ga_segments(id integer primary key, name varchar(1024), definition varchar(1024))');
 
 function get_ga_segments() {
+	global $ga_appname, $ga_p12_filepath, $ga_devacct;
+
 	$gclient = new Google_Client();
-	$gclient->setApplicationName("My Application");
-	$gkey = file_get_contents("ellipsis-4777e0661927.p12");
+	$gclient->setApplicationName($ga_appname);
+	$gkey = file_get_contents($ga_p12_filepath);
 	$gcred = new Google_Auth_AssertionCredentials(
-		"268295878612-lock95d09vrpkd5ehffpb16rusa7v7jv@developer.gserviceaccount.com",
+		"$ga_devacct",
 		array('https://www.googleapis.com/auth/analytics', 'https://www.googleapis.com/auth/analytics.edit', 'https://www.googleapis.com/auth/analytics.readonly'),
 		$gkey
 	);
@@ -25,28 +35,31 @@ function get_ga_segments() {
 }
 
 function ubx_ellipsis_ga_segment_exists($id) {
+	global $ubx_es_endpoint_id, $ubx_es_endpoint_key;
+
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/endpoint/12481/segments/".$id);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer c950658d-6c2e-4c71-8585-f0b87c75ddb6"));
+	curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/endpoint/".$ubx_es_endpoint_id."/segments/".$id);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer ".$ubx_es_endpoint_key));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 	$resp = curl_exec($ch);
 	return json_decode($resp, true);
 }
 
 function ubx_ellipsis_ga_del_segment($id) {
-	global $db;
+	global $db, $ubx_es_endpoint_id, $ubx_shiro_key;
 	$db->query("delete from ga_segments where id = " . $id);
         
 	$ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/endpoint/12481/segments/".$id);
+        curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/endpoint/".$ubx_es_endpoint_id."/segments/".$id);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer shiro-062c516b-1bce-4503-a4ef-80e37696bf25"));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer ".$ubx_shiro_key));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_exec($ch);
 }
 
 function ubx_ellipsis_ga_add_segment($name, $definition) {
-	global $db;
+	global $db, $ubx_es_endpoint_id, $ubx_es_endpoint_key;
+
 	$db->query("insert into ga_segments (name, definition) values ('".$name."', '".$definition."')");
 	$res = $db->query("select * from ga_segments where name = '".$name."'");
 	$id = -1;
@@ -63,23 +76,26 @@ function ubx_ellipsis_ga_add_segment($name, $definition) {
 					)
 	));
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/endpoint/12481/segments");
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer c950658d-6c2e-4c71-8585-f0b87c75ddb6", "Content-Type: application/json"));
+	curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/endpoint/".$ubx_es_endpoint_id."/segments");
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer ".$ubx_es_endpoint_key, "Content-Type: application/json"));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 	$resp = curl_exec($ch);
 }
 
 function ubx_ellipsis_get_jobs() {
+	global $ubx_es_endpoint_id, $ubx_es_endpoint_key;
+
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/jobs/SEGMENT_EXPORT?endpointId=12481&status=WAITING_TO_RECIEVE_DATA");
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer c950658d-6c2e-4c71-8585-f0b87c75ddb6", "Content-Type: application/json"));
+	curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/jobs/SEGMENT_EXPORT?endpointId=".$ubx_es_endpoint_id."&status=WAITING_TO_RECIEVE_DATA");
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer ".$ubx_es_endpoint_key, "Content-Type: application/json"));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$resp = curl_exec($ch);
 	return json_decode($resp, true);
 }
 
 function ubx_ellipsis_push_data($job) {
+	global $ubx_shiro_key;
 	//echo json_encode($job) . "\n\n";
 	$data = array(
 		"endpointID" => $job['producerEndpointID'],
@@ -92,7 +108,7 @@ function ubx_ellipsis_push_data($job) {
 	//echo json_encode($data) . "\n\n";
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "https://api-01.ubx.ibmmarketingcloud.com/v1/jobs/".$job['jobId']."/data");
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer shiro-062c516b-1bce-4503-a4ef-80e37696bf25", "Content-Type: application/json"));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer ".$ubx_shiro_key, "Content-Type: application/json"));
 	//curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer c950658d-6c2e-4c71-8585-f0b87c75ddb6", "Content-Type: application/json"));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	//curl_setopt($ch, CURLOPT_VERBOSE, 1);
